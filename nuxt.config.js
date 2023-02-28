7814ceb6f48dcc3b8eb0e441ae62ab414854c49e
Taishi-Ito <taishi.ito1026@gmail.com>
2023-02-28 16:56:01 +0900

    feat:Delete proxy and axios configuration

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,18 +51,6 @@ export default {
     '@nuxtjs/proxy',
   ],
 
-  proxy: {
-    '/api': {
-      target: 'http://localhost:8081/'
-    }
-  },
-
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {
-    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
-  },
-
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
