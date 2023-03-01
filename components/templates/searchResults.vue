435a87a4d62beecdce5043ab02e48058597dfb8b
Taishi-Ito <taishi.ito1026@gmail.com>
2023-03-01 18:48:32 +0900

    feat:Create component to show search results

---
+++ components/templates/searchResults.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2 class="result-title" v-if="similarMovies.length > 0">検索結果</h2>
+    <div v-for="movie in similarMovies">
+      <MoleculesResultCard
+        :title="movie.Title"
+        :CosineSimilarity="movie.CosineSimilarity"
+        :year="movie.Year"
+      ></MoleculesResultCard>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    similarMovies() {
+      return this.$store.getters["store/similarMovies"]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.result-title {
+  text-align: center;
+  margin-top: 50px;
+}
+</style>
